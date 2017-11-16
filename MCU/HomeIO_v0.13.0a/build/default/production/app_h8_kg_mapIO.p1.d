de2c445529a4ea63f7eb279ff0f95965e1a9f3df
Axel <axel@hieber.cc>
2017-11-16 09:52:14 +0100

    del

--- MCU/HomeIO_v0.13.0a/build/default/production/app_h8_kg_mapIO.p1.d
+++
@@ -1,7 +0,0 @@
- build/default/production/app_h8_kg_mapIO.d  \
- build/default/production/app_h8_kg_mapIO.p1:  \
- app_h8_kg_mapIO.c  \
-mcc_generated_files/eusart2.h  \
-uartString.h  \
-externvar.h  \
-data_types.h 
--- MCU/HomeIO_v0.13.0a/build/default/production/hadware_interface.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/hadware_interface.d  \
- build/default/production/hadware_interface.p1:  \
- hadware_interface.c  \
-data_types.h 
